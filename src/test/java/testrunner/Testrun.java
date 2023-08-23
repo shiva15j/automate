248e048c1228f0c7b4a89b2b4a0a651e41e20882
shivam <shivamsri@gmail.com>
2023-08-23 12:26:27 +0530

    comment

--- src/test/java/testrunner/Testrun.java
+++ src/test/java/testrunner/Testrun.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = ".//Features/Customer.feature",
+@CucumberOptions(features = ".//Features/Login.feature",
 glue = "StepDefination",
 dryRun = false,
 monochrome = false,
